c2a14d12d8db52944dd3990a90750bc1ac3be9c2
TEK Student <TEKstudent@vlab.lan>
2025-10-08 11:57:53 -0700

    uncommenting module

--- main.tf
+++ main.tf
@@ -130,8 +130,8 @@ module "ec2_instances" {
   }
 }
 
-# module "aws_s3_bucket" {
-#   source  = "app.terraform.io/policy-as-code-training/nat/aws"
-#   version = "1.0.0"
-#   bucket_name = "my-bucket-nat"
-# }
+module "aws_s3_bucket" {
+  source  = "app.terraform.io/policy-as-code-training/nat/aws"
+  version = "1.0.0"
+  bucket_name = "my-bucket-nat"
+}
