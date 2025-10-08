a908beda4b4d0412d172edaa2795198830715daa
TEK Student <TEKstudent@vlab.lan>
2025-10-08 10:54:47 -0700

    Add s3_bucket module from private registry

--- main.tf
+++ main.tf
@@ -129,3 +129,9 @@ module "ec2_instances" {
     environment = "development"
   }
 }
+
+module "s3_bucket" {
+  source  = "app.terraform.io/policy-as-code-training/s3-bucket-nat/aws"
+  version = "1.0.0"
+  bucket_name = "my-bucket"
+}
